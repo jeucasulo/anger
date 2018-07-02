5824bc80a821ef5ed02ab61bdecf2baf695f45c1
Jeú Guimarães Conrado Junior <jeucasulo@hotmail.com>
2018-07-02 02:33:23 -0300

    User Model Trait done. JS replaced by user model function in Role and Permission Views

--- app/User.php
+++ app/User.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Traits\Anger;
+
 class User extends Authenticatable
 {
     use Notifiable;
+    use Anger;
 
     /**
      * The attributes that are mass assignable.
@@ -28,47 +31,47 @@ class User extends Authenticatable
     ];
 
 
-    public function roles(){
-        //dd($this->belongsToMany(\App\Role::class));
-        return $this->belongsToMany(\App\Role::class);
-        //return $this->hasMany('\App\Role','id');
-    }
+    // public function roles(){
+    //     //dd($this->belongsToMany(\App\Role::class));
+    //     return $this->belongsToMany(\App\Role::class);
+    //     //return $this->hasMany('\App\Role','id');
+    // }
 
 
 
 
 
 
-    public function hasPermission(Permission $permission){ //get the permission and find the Manager,Adm,Editor and send it to hasAnyRole
+    // public function hasPermission(Permission $permission){ //get the permission and find the Manager,Adm,Editor and send it to hasAnyRole
         
-        return $this->hasAnyRoles($permission->roles); // uses the function roles() above
-    }
+    //     return $this->hasAnyRoles($permission->roles); // uses the function roles() above
+    // }
 
-    public function hasAnyRoles($permission_roles){ // get the Manager, Adm, Editor and check if the User->roles() created above has this same 'name'
+    // public function hasAnyRoles($permission_roles){ // get the Manager, Adm, Editor and check if the User->roles() created above has this same 'name'
 
-        if (is_array($permission_roles) || is_object($permission_roles)) {
-            foreach ($permission_roles as $role) {
-                if($this->roles->contains('name',$role->name)){
-                    return true;
-                }
-            } // use this OR the return bellow
-            //return !! $permission_roles->intersect($this->roles)->count(); // use this OR the FOREACH above
-        }
+    //     if (is_array($permission_roles) || is_object($permission_roles)) {
+    //         foreach ($permission_roles as $role) {
+    //             if($this->roles->contains('name',$role->name)){
+    //                 return true;
+    //             }
+    //         } // use this OR the return bellow
+    //         //return !! $permission_roles->intersect($this->roles)->count(); // use this OR the FOREACH above
+    //     }
 
-        return $this->roles->contains('name',$permission_roles);
-    }
-
-
+    //     return $this->roles->contains('name',$permission_roles);
+    // }
 
 
-    //checks if the the user has the received role
-    public function hasRole($role){
+
+
+    // //checks if the the user has the received role
+    // public function hasRole($role){
         
-            if($this->roles->contains('name',$role->name)){
-                return true;
-            }
-        // return false;
-    }
+    //         if($this->roles->contains('name',$role->name)){
+    //             return true;
+    //         }
+    //     // return false;
+    // }
 
 
 
